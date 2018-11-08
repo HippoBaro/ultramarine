647775e8d59d31fefa9366b5cdf2c39011adc30c
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-11-08 11:31:21 +0800

    Dummy ultramarine lib with Seastar interfacing

--- src/dummy.hpp
+++ src/dummy.hpp
@@ -1,18 +1,18 @@
 /*
  * MIT License
- *
+ * 
  * Copyright (c) 2018 Hippolyte Barraud
- *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,14 +22,6 @@
  * SOFTWARE.
  */
 
-#include "core/app-template.hh"
-#include "dummy.hpp"
+#pragma once
 
-int main(int ac, char** av) {
-    seastar::app_template app;
-
-    return app.run(ac, av, [&] {
-        say_hello();
-        return seastar::make_ready_future<int>(EXIT_SUCCESS);
-    });
-}
+void say_hello();
