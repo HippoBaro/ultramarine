f20a48ca7bd89e2c53e8c005360efef66fb6e83c
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2018-12-14 16:03:41 +0800

    actor attributes now lives in a separate header file

--- include/ultramarine/actor_attributes.hpp
+++ include/ultramarine/actor_attributes.hpp
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2018 Hippolyte Barraud
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,34 +24,19 @@
 
 #pragma once
 
-#include <memory>
-#include <unordered_map>
-#include <boost/core/noncopyable.hpp>
-#include "impl/macro.hpp"
+#include <limits>
 
 namespace ultramarine {
 
-    using actor_id = std::size_t;
-    using actor_activation_id = unsigned int;
+    namespace impl {
+        struct local_actor {
+        };
+    }
 
-    template <typename Actor>
-    using directory = std::unordered_map<actor_id, Actor>;
-
-    template <typename Actor>
-    using ActorKey = typename Actor::KeyType;
-
-    template<typename Actor>
-    struct vtable {
-        static constexpr auto table = Actor::message::make_vtable();
+    template<std::size_t ConcurrencyLimit = std::numeric_limits<std::size_t>::max()>
+    struct local_actor : public impl::local_actor {
+        static constexpr std::size_t max_activations = ConcurrencyLimit;
     };
 
-    enum class ActorKind {
-        SingletonActor,
-        LocalActor
-    };
-
-    class actor : private boost::noncopyable {
-    public:
-        using KeyType = actor_id;
-    };
+    struct non_reantrant_actor {};
 }
