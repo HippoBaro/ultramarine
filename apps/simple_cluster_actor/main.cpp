82c5902675afe8fb039b2c471ba281e215292cb1
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2019-05-11 03:34:18 +0800

    Switch cluster example to new wip API

--- apps/simple_cluster_actor/main.cpp
+++ apps/simple_cluster_actor/main.cpp
@@ -37,39 +37,6 @@ std::chrono::time_point<std::chrono::system_clock> end;
 
 auto range = boost::irange(0, 1000000);
 
-int main(int ac, char **av) {
-    seastar::app_template app;
-
-    fmt::print("actor_ref size: {}\n", sizeof(ultramarine::actor_ref<simple_actor>));
-    fmt::print(" -- local_actor_ref size: {}\n", sizeof(ultramarine::impl::collocated_actor_ref<simple_actor>));
-    fmt::print(" -- remote_actor_ref size: {}\n", sizeof(ultramarine::cluster::impl::remote_actor_ref<simple_actor>));
-
-    fmt::print("actor size: {}\n", sizeof(simple_actor));
-    fmt::print(" -- key size: {}\n", sizeof(simple_actor::KeyType));
-
-    return app.run(ac, av, [] {
-        return ultramarine::cluster::with_cluster(ultramarine::cluster::node(5556), { {"127.0.0.1", 5555} }, [](auto &s) {
-            start = std::chrono::system_clock::now();
-            return ultramarine::with_buffer(10000, [](auto &buffer) {
-                return seastar::do_for_each(range.begin(), range.end(), [&buffer](int i) {
-                    return buffer(ultramarine::get<simple_actor>(0)->say_hello());
-                });
-            }).then([] {
-                using namespace std::chrono;
-                end = std::chrono::system_clock::now();
-                auto span = end - start;
-                fmt::print("{} mes/sec\n", (float) (*range.end()) * 2 /
-                                           ((float) duration_cast<milliseconds>(span).count() / 1000));
-                auto request = sizeof(uint64_t) + sizeof(int64_t) + sizeof(uint32_t) + sizeof(ultramarine::actor_id);
-                auto response = sizeof(uint64_t) + sizeof(uint32_t);
-                fmt::print("{} B/sec\n", uint((request * *range.end() + response * *range.end()) /
-                                              ((double) duration_cast<milliseconds>(span).count() / 1000)));
-            });
-        });
-    });
-}
-
-
 //int main(int ac, char **av) {
 //    seastar::app_template app;
 //
@@ -81,8 +48,45 @@ int main(int ac, char **av) {
 //    fmt::print(" -- key size: {}\n", sizeof(simple_actor::KeyType));
 //
 //    return app.run(ac, av, [] {
-//        return ultramarine::cluster::with_cluster(5555, { }, [](auto const &service) {
-//            return seastar::sleep(std::chrono::hours(10));
+//        std::vector<seastar::socket_address> peers{
+//                seastar::socket_address(seastar::net::inet_address("127.0.0.1"), 5555U)
+//        };
+//
+//        return ultramarine::cluster::with_cluster(
+//                seastar::socket_address(seastar::net::inet_address("127.0.0.1"), 5556U), std::move(peers), []() {
+//                    seastar::print("In user code\n");
+//                    start = std::chrono::system_clock::now();
+//                    return ultramarine::with_buffer(10000, [](auto &buffer) {
+//                        return seastar::do_for_each(range.begin(), range.end(), [&buffer](int i) {
+//                            return buffer(ultramarine::get<simple_actor>(0)->say_hello());
+//                        });
+//                    }).then([] {
+//                        using namespace std::chrono;
+//                        end = std::chrono::system_clock::now();
+//                        auto span = end - start;
+//                        fmt::print("{} mes/sec\n", (float) (*range.end()) * 2 /
+//                                                   ((float) duration_cast<milliseconds>(span).count() / 1000));
+//                    });
+//                }).then([] {
+//            seastar::print("Out of user code\n");
 //        });
 //    });
 //}
+
+
+int main(int ac, char **av) {
+    seastar::app_template app;
+
+    fmt::print("actor_ref size: {}\n", sizeof(ultramarine::actor_ref<simple_actor>));
+    fmt::print(" -- local_actor_ref size: {}\n", sizeof(ultramarine::impl::collocated_actor_ref<simple_actor>));
+    fmt::print(" -- remote_actor_ref size: {}\n", sizeof(ultramarine::cluster::impl::remote_actor_ref<simple_actor>));
+
+    fmt::print("actor size: {}\n", sizeof(simple_actor));
+    fmt::print(" -- key size: {}\n", sizeof(simple_actor::KeyType));
+
+    return app.run(ac, av, [] {
+        return ultramarine::cluster::with_cluster(seastar::socket_address(seastar::net::inet_address("127.0.0.1"), 5555U), { }, [] {
+            return seastar::sleep(std::chrono::hours(10));
+        });
+    });
+}
