e58362f9e9b69fa8319d7aa38b84a5decd3a6289
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2019-05-30 20:12:15 +0800

    Improve run_cluster_bench to allow instances to have more than one core

--- benchmarks/run_cluster_bench.py
+++ benchmarks/run_cluster_bench.py
@@ -25,36 +25,50 @@ def find_bench(name):
     exit(1)
 
 
-def invoke_initiator(executable, cpu, port):
-    return subprocess.Popen(["./" + executable,
-                             "--smp", "1",
-                             "--cpuset", str(cpu),
-                             "-l", "127.0.0.1:{}".format(port),
-                             "--minimum-peers", str(multiprocessing.cpu_count() - 1),
-                             "--initiator", "1"])
+def invoke_initiator(executable, cpus, port):
+    cpustr = ','.join(map(str, cpus))
+
+    invocation = ["./" + executable,
+                  "--smp", str(len(cpus)),
+                  "--cpuset", cpustr,
+                  "-l", "127.0.0.1:{}".format(port),
+                  "--minimum-peers", str(multiprocessing.cpu_count() / len(cpus) - 1),
+                  "--initiator", "1"]
+
+    print("\033[1m\033[94mStarting bootstrap instance: {}\033[0m".format(" ".join(invocation)))
+    return subprocess.Popen(invocation)
 
 
-def invoke_peer(executable, cpu, initiator_addr, port):
-    return subprocess.Popen(["./" + executable,
-                             "--smp", "1",
-                             "--cpuset", str(cpu),
-                             "-l", "127.0.0.1:{}".format(port),
-                             "--peers", initiator_addr])
+def invoke_peer(executable, cpus, initiator_addr, port):
+    cpustr = ','.join(map(str, cpus))
+    invocation = ["./" + executable,
+                  "--smp", str(len(cpus)),
+                  "--cpuset", cpustr,
+                  "-l", "127.0.0.1:{}".format(port),
+                  "--minimum-peers", str(multiprocessing.cpu_count() / len(cpus) - 1),
+                  "--peers", initiator_addr]
+
+    print("\033[1m\033[94mStarting instance: {}\033[0m".format(" ".join(invocation)))
+    return subprocess.Popen(invocation)
 
 
 def main(arguments):
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-b', '--benchmark', help="Benchmark name to run")
+    parser.add_argument('-c', '--smp', help="Number of thread per instance. Must be divisible by system cpu count",
+                        type=int, default=1)
     args = parser.parse_args(arguments)
 
     executable = find_bench(args.benchmark)
 
     initiator_port = 5000
-    initiator = invoke_initiator(executable, 0, initiator_port)
+    initiator = invoke_initiator(executable, list(range(0, args.smp)), initiator_port)
 
     peers = list()
-    for i in range(1, multiprocessing.cpu_count()):
-        peers.append(invoke_peer(executable, i, "127.0.0.1:{}".format(initiator_port), 5000 + i))
+
+    for i in range(args.smp, multiprocessing.cpu_count(), args.smp):
+        peers.append(
+            invoke_peer(executable, list(range(i, i + args.smp)), "127.0.0.1:{}".format(initiator_port), 5000 + i))
 
     initiator.wait()
 
